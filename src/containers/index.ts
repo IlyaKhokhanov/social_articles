79ce5dfc88e6e4584c09a28763c71a9cb719d0db
IlyaKhokhanov <hohanov.ilya@gmail.com>
2024-10-12 21:15:29 +0300

    feat: list articles

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export { SignupForm } from './forms/signupForm';
 export { SigninForm } from './forms/signinForm';
 export { ChangeForm } from './forms/changeForm';
+export { ListArticles } from './listArticles/listArticles';
---
+++ src/containers/listArticles/listArticles.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { ActionButtons, Button } from '@/components';
+import { useAppSelector } from '@/redux/hooks';
+import { IArticle } from '@/types';
+
+import styles from './listArticles.module.css';
+
+export const ListArticles = ({ data }: { data: IArticle[] }) => {
+  const [username, setUsername] = useState<string | null>(null);
+  const { user } = useAppSelector((state) => state.app);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) setUsername(user);
+  }, [user]);
+
+  const addArticleHandle = () => {
+    router.push('/write');
+  };
+
+  return (
+    <div className={styles.container}>
+      {username && <ActionButtons editBtn={{ cb: addArticleHandle, text: 'Написать статью' }} />}
+      <div className={styles.articles}>
+        {data.map((el) => (
+          <div className={styles.card} key={uuidv4()}>
+            <h2 className={styles.title}>{el.title}</h2>
+            {el.image ? (
+              <Image
+                className={styles.img}
+                src={`${el.image}`}
+                alt="logo"
+                unoptimized
+                width={300}
+                height={150}
+                priority
+              />
+            ) : (
+              <div className={styles.empty}>Картинка отсутствует</div>
+            )}
+
+            <div className={styles.text}>
+              {el.content.substring(0, 50)}
+              <Link className={styles.link} href={`/${el.id}`}>
+                <Button className={styles.readMore}>Читать далее</Button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
