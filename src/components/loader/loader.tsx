95607b130f6a2cbc621c6f8d32d84b3713dd9daa
IlyaKhokhanov <hohanov.ilya@gmail.com>
2024-10-14 12:43:41 +0300

    fix: fixed loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -7,7 +7,7 @@ import styles from './loader.module.css';
 export const Loader = () => {
   return (
     <div className={styles.loader}>
-      <Image src={loaderImage} alt="await" width={200} height={200} priority unoptimized />
+      <Image src={loaderImage} alt="await" width={200} height={200} priority />
     </div>
   );
 };
